76ab9e659b8090358ebb95ff2aecf4c326d7edb9
e.buckel@orange.fr <e.buckel@orange.fr>
2021-03-17 11:26:56 +0100

    reload

--- code2.js
+++ code2.js
@@ -1,35 +1,94 @@
 "use.strict";
 
 fetch("./folder.json")
-    .then(function(resp) {
+    .then(function (resp) {
         return resp.json();
     })
-    .then(function(data){
+    .then(function (data) {
         index(data.photographers);
     });
 
+function reload() {
+    var tag = this.innerHTML;
+    console.log(tag);
+
+    // const tag = document.getElementsByClassName("link");
+    // const temp_tag = Object.values(tag);
+    // console.log(temp_tag);
+    // temp_tag.forEach(link => {
+    //     link.addEventListener("click", active_tag)
+    // });
+
+    active_tag(tag.toLowerCase());
+
+    fetch("./folder.json")
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (data) {
+            index(data.photographers);
+        });
+
+};
+
+function active_tag(tag) {
+    const tag1 = document.getElementsByClassName("link");
+    const temp_tag1 = Object.values(tag1);
+    // console.log(temp_tag1[0]);
+    for (var i = 0; i < temp_tag1.length; i++) {
+        if (temp_tag1[i].innerHTML.toLowerCase() == tag) {
+            console.log(temp_tag1[i].classList.contains("active_tag"))
+            // if (temp_tag1[i].classList.contains("active_tag")) {
+                temp_tag1[i].setAttribute("class", "active_tag link");
+            // } else {
+                // temp_tag1[i].setAttribute("class", "link");
+            // }
+            console.log(temp_tag1[i]);
+        }
+    }
+}
+
+
+// const button_test = document.getElementsByClassName("button_lvl_selector");
+// const temp = Object.values(button_test);
+
+// temp.forEach(link => {
+//     link.addEventListener("click", calcul)
+// });
+
+
+
 function index(photographers) {
     const number_photographer = photographers.length;
-    for (i=0;i<number_photographer;i++) {
+    const placeto = document.getElementById("collection_photographes");
+    if (placeto !== null) {
+        placeto.innerHTML = "";
+    }
+    for (i = 0; i < number_photographer; i++) {
         // console.log(photographers[i]);
         create_carte_photographe(photographers[i]);
     }
+    const tag = document.getElementsByClassName("link");
+    const temp_tag = Object.values(tag);
+    temp_tag.forEach(link => {
+        link.addEventListener("click", reload)
+    });
 };
 
 function create_carte_photographe(photographe) {
-    let a,b,b0,b1,c,c0,c1,c2,d,d0;
+    let a, b, b0, b1, c, c0, c1, c2, d, d0;
     a = document.createElement("DIV");
-    a.setAttribute("class","container_ALL");
+    a.setAttribute("class", "container_ALL");
 
     b = document.createElement("A");
-    b.setAttribute("class","under_part_img");
-    b.setAttribute("href","");
+    b.setAttribute("class", "under_part_img");
+    b.setAttribute("href", "");
     let indice = "";
 
-    setAttributes(b,{"class": "under_part_img", "href": "page_2.html?photographe="+photographe.id})
+    setAttributes(b, { "class": "under_part_img", "href": "page_2.html?photographe=" + photographe.id })
 
     b0 = document.createElement("DIV");
-    setAttributes(b0,{"class": "square","style": "background-image: url('Sample_Photos/Photographers_ID_Photos/"+photographe.portrait+"')"});
+    setAttributes(b0, { "class": "square", "style": "background-image: url('Sample_Photos/Photographers_ID_Photos/" + photographe.portrait + "')" });
     b1 = document.createElement("H2");
     b1.setAttribute("class", "index_photographe");
     b1.innerHTML = photographe.name;
@@ -37,24 +96,24 @@ function create_carte_photographe(photographe) {
     b.appendChild(b1);
 
     c = document.createElement("DIV");
-    c.setAttribute("class","under_part_txt");
+    c.setAttribute("class", "under_part_txt");
     c0 = document.createElement("H3");
-    c0.innerHTML = photographe.city +", "+photographe.country;    
+    c0.innerHTML = photographe.city + ", " + photographe.country;
     c1 = document.createElement("p");
-    c1.innerHTML = photographe.tagline;    
+    c1.innerHTML = photographe.tagline;
     c2 = document.createElement("p");
     c2.setAttribute("class", "price");
-    c2.innerHTML = photographe.price+"€/jour";
+    c2.innerHTML = photographe.price + "€/jour";
     c.appendChild(c0);
     c.appendChild(c1);
     c.appendChild(c2);
 
     d = document.createElement("DIV");
-    d.setAttribute("class","under_part_link");
+    d.setAttribute("class", "under_part_link");
     photographe.tags.forEach(element => {
         d0 = document.createElement("P");
         d0.setAttribute("class", "link");
-        d0.innerHTML="#"+element;
+        d0.innerHTML = "#" + element;
         d.appendChild(d0);
     });
 
@@ -63,10 +122,11 @@ function create_carte_photographe(photographe) {
     a.appendChild(d);
     // console.log(a);
 
-    
+
     const placeto = document.getElementById("collection_photographes");
-    if (placeto !== null){
-    placeto.appendChild(a);
+    if (placeto !== null) {
+        // placeto.innerHTML = "";
+        placeto.appendChild(a);
     }
 };
 
@@ -74,7 +134,7 @@ function image() {
 
 };
 
-function setAttributes(el,attrs) {
+function setAttributes(el, attrs) {
     for (var key in attrs) {
         el.setAttribute(key, attrs[key]);
     }
