2a07a52da2c7fa724c7a46ec83308c6c67367ab2
e.buckel@orange.fr <e.buckel@orange.fr>
2021-03-18 23:16:53 +0100

    page 2 galerie => fonction galerie

--- galerie.js
+++ galerie.js
@@ -1,11 +1,33 @@
-const media = document.getElementsByClassName("active_tag");
+const media = document.getElementsByClassName("clickable_galerie");
 const media_array = Object.values(media);
 
 media_array.forEach(elt => {
-    elt.addEventListener("click", galerie)
+    elt.addEventListener("click", open_galerie)
 });
 
+//Ouvre la galerie
+function open_galerie() {
+    this.
 
-function galerie() {
-
+    build_picture_galerie(photographer_id, id)
 };
+
+//Construit la photo/vidéo demandée
+function build_picture_galerie(photographer_id, id) {
+    fetch()
+}
+
+
+
+fetch("./folder.json")
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        let tag = find_tag();
+        if (tag != false) {
+            active_tag(tag.toLowerCase());
+        }
+        index(data.photographers, tag);
+        
+    });
